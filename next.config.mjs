1020d390c1aed2439f7e600e438fb9b4225c8e43
joelerich08-prog <joelerich08@gmail.com>
2026-04-15 10:25:44 +0800

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next-build',
+  distDir: '.next-clean',
   images: {
     unoptimized: true,
   },
